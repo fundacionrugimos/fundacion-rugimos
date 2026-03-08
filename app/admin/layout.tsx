a979ca893abae3cb940e6546313b4d44627c4c22
Pablo Lucas <rugimos.fundacionsfl@gmail.com>
2026-03-08 13:18:26 -0400

    desactivar login temporalmente

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,31 +1,8 @@
-"use client"
-
-import { useEffect } from "react"
-import { useRouter, usePathname } from "next/navigation"
-
 export default function AdminLayout({
 children,
 }:{
 children: React.ReactNode
 }){
-
-const router = useRouter()
-const pathname = usePathname()
-
-useEffect(()=>{
-
-// permitir página de login
-if(pathname === "/admin/login") return
-
-const logado = localStorage.getItem("admin_logged")
-
-if(!logado){
-
-router.replace("/admin/login")
-
-}
-
-},[pathname,router])
 
 return <>{children}</>
 
